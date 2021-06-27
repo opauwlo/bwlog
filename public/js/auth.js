be02ca0fb6678e93bb71089866cf46cc1bf5d857
opauwlo <opvcfreitas@gmail.com>
2021-06-27 16:56:22 -0300

    bug fix -login

--- public/js/auth.js
+++ public/js/auth.js
@@ -7,7 +7,7 @@ function SignIn(googleUser) {
   xhr.onload = function() {
     console.log('Signed in as: ' + xhr.responseText);
     if (xhr.responseText ==  'success'){
-    }
+      location.assign('/perfil')    }
   };
   xhr.send(JSON.stringify({token: id_token}));
   
