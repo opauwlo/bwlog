0e876f828cdbf2ae71d21b487e016ed0782c5da4
opauwlo <opvcfreitas@gmail.com>
2021-06-30 18:06:21 -0300

    --fix readme and index.js

---
+++ README.md
@@ -0,0 +1,98 @@
+<div align="center" id="top"> 
+  <img src="./.github/app.gif" alt="social-blog" />
+
+  &#xa0;
+
+  <!-- <a href="https://social-blog.netlify.app">Demo</a> -->
+</div>
+
+<h1 align="center">social-blog</h1>
+
+<p align="center">
+  <img alt="Github top language" src="https://img.shields.io/github/languages/top/opauwlo/social-blog?color=56BEB8">
+
+  <img alt="Github language count" src="https://img.shields.io/github/languages/count/opauwlo/social-blog?color=56BEB8">
+
+  <img alt="Repository size" src="https://img.shields.io/github/repo-size/opauwlo/social-blog?color=56BEB8">
+
+  <img alt="License" src="https://img.shields.io/github/license/opauwlo/social-blog?color=56BEB8">
+
+  <!-- <img alt="Github issues" src="https://img.shields.io/github/issues/opauwlo/social-blog?color=56BEB8" /> -->
+
+  <!-- <img alt="Github forks" src="https://img.shields.io/github/forks/opauwlo/social-blog?color=56BEB8" /> -->
+
+  <!-- <img alt="Github stars" src="https://img.shields.io/github/stars/opauwlo/social-blog?color=56BEB8" /> -->
+</p>
+
+<!-- Status -->
+
+<!-- <h4 align="center"> 
+	🚧  social-blog 🚀 Under construction...  🚧
+</h4> 
+
+<hr> -->
+
+<p align="center">
+  <a href="#dart-about">About</a> &#xa0; | &#xa0; 
+  <a href="#sparkles-features">Features</a> &#xa0; | &#xa0;
+  <a href="#rocket-technologies">Technologies</a> &#xa0; | &#xa0;
+  <a href="#white_check_mark-requirements">Requirements</a> &#xa0; | &#xa0;
+  <a href="#checkered_flag-starting">Starting</a> &#xa0; | &#xa0;
+  <a href="#memo-license">License</a> &#xa0; | &#xa0;
+  <a href="https://github.com/opauwlo" target="_blank">Author</a>
+</p>
+
+<br>
+
+## :dart: About ##
+
+Describe your project
+
+## :sparkles: Features ##
+
+:heavy_check_mark: Feature 1;\
+:heavy_check_mark: Feature 2;\
+:heavy_check_mark: Feature 3;
+
+## :rocket: Technologies ##
+
+The following tools were used in this project:
+
+- [Expo](https://expo.io/)
+- [Node.js](https://nodejs.org/en/)
+- [React](https://pt-br.reactjs.org/)
+- [React Native](https://reactnative.dev/)
+- [TypeScript](https://www.typescriptlang.org/)
+
+## :white_check_mark: Requirements ##
+
+Before starting :checkered_flag:, you need to have [Git](https://git-scm.com) and [Node](https://nodejs.org/en/) installed.
+
+## :checkered_flag: Starting ##
+
+```bash
+# Clone this project
+$ git clone https://github.com/opauwlo/social-blog
+
+# Access
+$ cd social-blog
+
+# Install dependencies
+$ yarn
+
+# Run the project
+$ yarn start
+
+# The server will initialize in the <http://localhost:3000>
+```
+
+## :memo: License ##
+
+This project is under license from MIT. For more details, see the [LICENSE](LICENSE.md) file.
+
+
+Made with :heart: by <a href="https://github.com/opauwlo" target="_blank">{{YOUR_NAME}}</a>
+
+&#xa0;
+
+<a href="#top">Back to top</a>
--- index.js
+++ index.js
@@ -7,194 +7,205 @@ const random = require ('./public/js/slugNumbers')
 const seedrandom = require('seedrandom');
 
 // Google
-const CLIENT_ID = process.env.CLIENT_ID;
-const {OAuth2Client} = require('google-auth-library');
-const client = new OAuth2Client(CLIENT_ID);
-require('./public/js/auth')
-require("dotenv").config();
+  const CLIENT_ID = process.env.CLIENT_ID;
+  const {OAuth2Client} = require('google-auth-library');
+  const client = new OAuth2Client(CLIENT_ID);
+  require('./public/js/auth')
+  require("dotenv").config();
 
-// Config
-// Random
-rng = seedrandom('added entropy.', { entropy: true });
-//static
-var app = express();
-app.use(express.static(__dirname + "/public"));
-// Template
-app.engine(
-  "handlebars",
-  handlebars({
-    defaultLayout: "main",
-  })
-);
-app.set("view engine", "handlebars");
-// BodyParser
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-);
-app.use(express.json());
-app.use(cookieParser());
-// Rotas
-app.get(`/`, function(req, res) {
-  //
-  
-  Post.findAll({
-    order: [["id", "DESC"]],
-    where: {
-      publicado: true
-    }
-  }).then((posts)=> {
-    res.render("home", 
-    { 
-      posts: posts
-    });
-  });
-});
+// Configs
+  // Random
+    rng = seedrandom('added entropy.', { entropy: true });
 
-app.post(`/`, (req, res)=>{
-  let token = req.body.token
-  async function verify() {
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: CLIENT_ID,
+  //static
+    var app = express();
+    app.use(express.static(__dirname + "/public"));
 
-    }); 
-    const payload = ticket.getPayload();
-    const userid = payload['sub'];
-  }
-  verify()
-  .then(()=>{
-    res.cookie('session-token', token );
-    res.send('success');
-  }).catch(console.error);
-})
+  // Template
+    app.engine(
+      "handlebars",
+      handlebars({
+        defaultLayout: "main",
+      })
+    );
+    app.set("view engine", "handlebars");
 
-app.get('/login', (req, res)=>{
-  res.render('login');
-})
+  // BodyParser
+    app.use(
+      express.urlencoded({
+        extended: false,
+      })
+    );
+    app.use(express.json());
+    app.use(cookieParser());
 
-app.get('/logout', (req, res)=>{
-  res.clearCookie('session-token');
-  res.redirect('/login');
-})
+  // Rotas
+    //home
+      app.get(`/`, function(req, res) {
+        
+        Post.findAll({
+          order: [["id", "DESC"]],
+          where: {
+            publicado: true
+          }
+        }).then((posts)=> {
+          res.render("home", 
+          { 
+            posts: posts
+          });
+        });
+      });
 
-app.get('/perfil', checkAuthenticated, (req, res)=>{
-  let user = req.user;
-  Post.findAll({
-    order: [["id", "DESC"]],
-    where: {
-      id_user: user.sub
-    }
-  }).then((posts)=>{
-    res.render("perfil", {
-      posts: posts,
-      user: user
-    })
-  })
-})
+    //get login infos 
+      app.post(`/`, (req, res)=>{
+        let token = req.body.token
+        async function verify() {
+          const ticket = await client.verifyIdToken({
+              idToken: token,
+              audience: CLIENT_ID,
 
-app.get('/autor/:id_user', (req, res)=>{
-  const id = req.params.id_user;
-  Post.findAll({
-    order: [["id", "DESC"]],
-    where: {
-      id_user: id,
-      publicado: true
-    }
-  }).then((posts)=> {
-    res.render("autor", 
-    { 
-      posts: posts
-    });
-  });
-})
+          }); 
+          const payload = ticket.getPayload();
+          const userid = payload['sub'];
+        }
+        verify()
+        .then(()=>{
+          res.cookie('session-token', token );
+          res.send('success');
+        }).catch(console.error);
+      })
 
+    //login page
+      app.get('/login', (req, res)=>{
+        res.render('login');
+      })
 
-app.get(`/cad`, checkAuthenticated, function(req, res) {
-  let user = req.user;
-  res.render("form", {user});
-});
+    //logout for clean coockie session
+      app.get('/logout', (req, res)=>{
+        res.clearCookie('session-token');
+        res.redirect('/login');
+      })
+    //profile page
+      app.get('/perfil', checkAuthenticated, (req, res)=>{
+        let user = req.user;
+        Post.findAll({
+          order: [["id", "DESC"]],
+          where: {
+            id_user: user.sub
+          }
+        }).then((posts)=>{
+          res.render("perfil", {
+            posts: posts,
+            user: user
+          })
+        })
+      })
 
-app.post(`/add`, checkAuthenticated, function(req, res) {
-  Post.create({
-    autor: req.body.autor,
-    email: req.body.email,
-    foto: req.body.foto,
-    titulo: req.body.titulo,
-    slug: slugify(req.body.titulo + - + random(),{
-      lower: true
-    }),
-    descricao: req.body.descricao,
-    conteudo: req.body.conteudo,
-    publicado: req.body.publicado,
-    id_user: req.body.sub
-  })
-    .then(function() {
-      res.redirect(`/`);
-    })
-    .catch(function(erro) {
-      res.send(`falha ao criar post: ` + erro);
-    });
-});
+    //profile public page
+      app.get('/autor/:id_user', (req, res)=>{
+        const id = req.params.id_user;
+        Post.findAll({
+          order: [["id", "DESC"]],
+          where: {
+            id_user: id,
+            publicado: true
+          }
+        }).then((posts)=> {
+          res.render("autor", 
+          { 
+            posts: posts
+          });
+        });
+      })
 
-app.post(`/update/:id`, checkAuthenticated, function(req, res) {
-  const id = req.params.id;
+    //page to creat posts
+      app.get(`/cad`, checkAuthenticated, function(req, res) {
+        let user = req.user;
+        res.render("form", {user});
+      });
+    
+    //page to send your posts infos
+      app.post(`/add`, checkAuthenticated, function(req, res) {
+        Post.create({
+          autor: req.body.autor,
+          email: req.body.email,
+          foto: req.body.foto,
+          titulo: req.body.titulo,
+          slug: slugify(req.body.titulo + - + random(),{
+            lower: true
+          }),
+          descricao: req.body.descricao,
+          conteudo: req.body.conteudo,
+          publicado: req.body.publicado,
+          id_user: req.body.sub
+        })
+          .then(function() {
+            res.redirect(`/`);
+          })
+          .catch(function(erro) {
+            res.send(`falha ao criar post: ` + erro);
+          });
+      });
+    //page to show a post from somone else
+      app.get(`/posts/:slug`, function(req, res) {
+        Post.findAll({
+          where: {
+            slug: req.params.slug,
+          },
+        }).then(function(post) {
+          res.render("posts", {
+            posts: post,
+          });
+        });
+      });
+      
+    //page to insert new infos in some post
+      app.get(`/edit/:id`, checkAuthenticated, function(req, res) {
+        
+        Post.findAll({
+          where: {
+            id: req.params.id,
+          },
+        }).then((post, user)=> {
+          var user = req.user;
+          res.render("edit", {
+            edit: post,
+            info: {user}
+          });
+        });
+      });
+      
+    //page to post posts updates
+      app.post(`/update/:id`, checkAuthenticated, function(req, res) {
+        const id = req.params.id;
 
-  Post.update(req.body, {
-    where: {
-      id: id,
-    },
-  })
-    .then(function() {
-      res.redirect(`/perfil`);
-    })
-    .catch(function(erro) {
-      res.send(`falha ao atualizar post: ` + erro);
-    });
-});
+        Post.update(req.body, {
+          where: {
+            id: id,
+          },
+        })
+          .then(function() {
+            res.redirect(`/perfil`);
+          })
+          .catch(function(erro) {
+            res.send(`falha ao atualizar post: ` + erro);
+          });
+      });
 
-app.get(`/edit/:id`, checkAuthenticated, function(req, res) {
-  
-  Post.findAll({
-    where: {
-      id: req.params.id,
-    },
-  }).then((post, user)=> {
-    var user = req.user;
-    res.render("edit", {
-      edit: post,
-      info: {user}
-    });
-  });
-});
-
-app.get(`/deletar/:id`, function(req, res) {
-  Post.destroy({
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then(function() {
-      res.redirect(`/perfil`);
-    })
-    .catch(function(erro) {
-      res.send("Erro ao deletar posategem!" + erro);
-    });
-});
-
-app.get(`/posts/:slug`, function(req, res) {
-  Post.findAll({
-    where: {
-      slug: req.params.slug,
-    },
-  }).then(function(post) {
-    res.render("posts", {
-      posts: post,
-    });
-  });
-});
-
+    //page to delet some post
+      app.get(`/deletar/:id`, function(req, res) {
+        Post.destroy({
+          where: {
+            id: req.params.id,
+          },
+        })
+          .then(function() {
+            res.redirect(`/perfil`);
+          })
+          .catch(function(erro) {
+            res.send("Erro ao deletar posategem!" + erro);
+          });
+      });
 
 function checkAuthenticated(req, res, next){
 
