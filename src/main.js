0ccc183fbfd2eb8b84725f0d110279fec58137b6
opauwlo <opvcfreitas@gmail.com>
2021-11-26 22:43:12 -0300

    add PORT process.env

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const app = require('./configs/server.config');
+require('dotenv').config();
 
-app.listen(process.env.PORT || 8000, () => {
+app.listen(process.env.PORT, () => {
   console.log('running');
 });
