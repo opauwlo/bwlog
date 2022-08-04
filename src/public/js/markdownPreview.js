f52c804a201609811043eb8eb74fdbbd8a52d637
opauwlo <opvcfreitas@gmail.com>
2022-08-03 23:09:17 -0300

    ISSUE-23: [FEAT] Using EasyMD as default markdown editor

--- src/public/js/markdownPreview.js
+++ src/public/js/markdownPreview.js
@@ -24,15 +24,21 @@ function previewRender(uniqueID) {
   md.use(window.markdownitDeflist);
   md.use(window.markdownitIns);
   md.use(window.markdownitMark);
-  md.use(window.markdownitTaskLists);
-  
-  var simplemde = new SimpleMDE({ 
+  var simplemde = new EasyMDE({ 
     element: document.getElementById("textarea"),
     promptURLs: true,
     showIcons: ["code", "table"],
+    insertTexts: {
+      horizontalRule: ["", "\n\n-----\n\n"],
+      table: ["", "\n\n| Column 1 | Column 2 | Column 3 |\n| -------- | -------- | -------- |\n| Text     | Text     | Text     |\n\n"],
+    },
       renderingConfig: {
       singleLineBreaks: true,
       codeSyntaxHighlighting: true,
+      sanitizerFunction: (renderedHTML) => {
+        // Using DOMPurify and only allowing <b> tags
+        return DOMPurify.sanitize(renderedHTML, {ALLOWED_TAGS: ['b']});
+    },
     },
       autosave: {
       enabled: true,
@@ -41,18 +47,23 @@ function previewRender(uniqueID) {
     },
       previewRender: function(plainText, preview) { // Async method
       setTimeout(function(){
-        preview.innerHTML = DOMPurify.sanitize(md.render(plainText));
+        let renderer = md.render(plainText);
+        preview.innerHTML = renderer;
       }, 250);
       return "Loading...";
     },
     styleSelectedText: true,
     tabSize: 2,
-    toolbar: ['bold', 'italic', '|', 'link', 'image', 'code', '|', 'preview', 'fullscreen'],
+    toolbar: ['heading','bold', 'italic', '|', 'link', 'image', 'code', '|', 'preview', 'fullscreen'],
     lineWrapping: true,
-    forceSync: true,
     placeholder: '...',
+    styleSelectedText: false  ,
+    parsingConfig: {
+      allowAtxHeaderWithoutSpace: true,
+      strikethrough: true,
+      underscoresBreakWords: false,
+  },
    });
 
    return simplemde;
 }
-
