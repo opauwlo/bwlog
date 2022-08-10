423a8d2cb1b8ff325dce4663d12a40a5ea1e118d
opauwlo <opvcfreitas@gmail.com>
2022-08-09 22:35:40 -0300

    ISSUE-40: [FEAT] Change shared img post background

--- src/app/utils/creteSharedImg.js
+++ src/app/utils/creteSharedImg.js
@@ -1,21 +1,23 @@
-const { Canvas, resolveImage } = require("canvas-constructor/skia");
-
+const { Canvas, resolveImage, FontLibrary} = require("canvas-constructor/skia");
+FontLibrary.use("Nerd", ["./src/app/utils/fonts/NerdFont.ttf"]);
 
 async function createImg(str, imgProfile, nameProfile) {
 
-  const img = await resolveImage("https://res.cloudinary.com/bwlog/image/upload/v1651956912/essencials/bwlog-7_i8xnt6.png");
+  const img = await resolveImage("https://res.cloudinary.com/bwlog/image/upload/v1660094074/essencials/back_shared_lwspwa.png");
   imgProfile = await resolveImage(imgProfile);
-  let image = await new Canvas(600, 300)
-    .printImage(img, 0, 0, 600, 300)
-    .setTextFont("bold 28px Cinzel")
+  let image = await new Canvas(1280, 720)
+    .printImage(img, 0, 0, 1280, 720)
+    // set text color whitesmoke
+    .setColor("whitesmoke")
+    .setTextFont("bold 50px Nerd")
     .setTextAlign("center")
-    .printCircularImage(imgProfile, 55, 225, 27, 27)
-    .printWrappedText(str, 300, 135, 555, 30)
-    .setTextFont("bold 13px Cinzel")
+    .printCircularImage(imgProfile, 71, 620, 51, 51)
+    .printWrappedText(str, 640, 350, 1020, 35)
+    .setTextFont("bold 22px Nerd")
     .setTextBaseline("middle")
-    .printText(nameProfile, 119, 225)
+    .printText(nameProfile, 200, 637)
     .toDataURL();
     return image
-}
+} 
 
 module.exports = createImg
